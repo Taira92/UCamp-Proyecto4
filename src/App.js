35e65bb33594c98ebf683cd8f750d1797816212d
GustavoTairaToguchi <gtairatoguchi@gmail.com>
2022-01-24 23:36:29 -0500

    Mejoras

--- src/App.js
+++ src/App.js
@@ -28,6 +28,13 @@ function App() {
   const [precio, setPrecio] = useState({ precio: "" });
   const [url, setUrl] = useState({ url: "" });
 
+
+  const [cliente, setCliente] = useState({ cliente: "" });
+  const [correo, setCorreo] = useState({ correo: "" });
+  const [telefono, setTelefono] = useState({ telefono: "" });
+  const [fecha, setFecha] = useState({ fecha: "" });
+  const [hora, setHora] = useState({ hora: "" });
+
   const [esEditar, setEsEditar] = useState(false);
 
   const obtenerPlatos = async () => {
@@ -110,6 +117,27 @@ function App() {
 
   const handleFormUrl = (texto) => {
     setUrl(texto);
+  };
+
+
+  const handleFormCliente = (texto) => {
+    setCliente(texto);
+  };
+
+  const handleFormCorreo = (texto) => {
+    setCorreo(texto);
+  };
+
+  const handleFormTelefono= (texto) => {
+    setTelefono(texto);
+  };
+
+  const handleFormFecha = (texto) => {
+    setFecha(texto);
+  };
+
+  const handleFormHora = (texto) => {
+    setHora(texto);
   };
 
   const editarPlato = (item) => {
@@ -313,7 +341,76 @@ function App() {
             </div>
           </Route>
           <Route path="/" exact>
-            <Inicio onSubmit={guardarReservaSubmit} />
+          <div id="inicio">
+    <div id="index" className="row">
+      <div className="col-sm-6 col-xs-12">
+          <img className='inicio-img' src="https://img-global.cpcdn.com/recipes/db3c4c121ae53201/1200x630cq70/photo.jpg" alt="" />
+      </div>
+      <div className="col-sm-6 col-xs-12 inicio-intro">
+        <div>
+          <h1>Criollón</h1> 
+          <p>Somos un restaurante criollo de comida peruana, caracterizados por la alta calidad y variedad de nuestros platos.</p>
+        </div>
+      </div>
+    </div>
+    <div id="inicio-reserva" className="row">
+
+        <div className="col-sm-6 col-xs-12">
+            <h2>Haz tu reserva!</h2>
+            <p>Ten un momento agradable con la mejor comida ya sea solo o acompañado. Para ello, ingresa tus datos indicandonos el dia y la hora de tu reserva.</p>
+        </div>
+
+        <div className="col-sm-6 col-xs-12">
+              <h2>Registrar reserva</h2>         
+              <form onSubmit={guardarReservaSubmit}>
+                <label className="form-label">Nombres y apellidos</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => handleFormCliente(e.target.value)}
+                  value={cliente}
+                />
+
+                <label className="form-label">Correo</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  onChange={(e) => handleFormCorreo(e.target.value)}
+                  value={correo}
+                />
+
+                <label className="form-label">Teléfono</label>
+                <input
+                  type="tel"
+                  className="form-control"
+                  onChange={(e) => handleFormTelefono(e.target.value)}
+                  value={telefono}
+                />
+
+                <label className="form-label">Fecha</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  onChange={(e) => handleFormFecha(e.target.value)}
+                  value={fecha}
+                /> 
+                
+                <label className="form-label">Hora</label>
+                <input
+                type="time"
+                className="form-control"
+                onChange={(e) => handleFormHora(e.target.value)}
+                value={hora}
+              />
+
+                <button id="form-submit" type="submit" className="btn btn-dark">
+                  Agregar tarea
+                </button>
+              </form>
+          </div>
+      </div>
+
+    </div>
           </Route>
         </Switch>
       </div>
